15c786aa7425f92118374fdc35cb3e8cad75b1eb
Don Cooper <don_cooper@hotmail.com>
2017-06-28 16:22:15 -0400

    Request returning id, name and types

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,7 +3,11 @@ class PokemonController < ApplicationController
   def show
     res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}", followlocation: true)
     body = JSON.parse(res.body)
-    render json: body["name"]
+    render json: {
+      id:     body["id"],
+      name:   body["name"],
+      types:  body["types"]
+    }
   end
 
 end
