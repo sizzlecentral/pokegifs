0facac94eb532e3a77c90ec719fd343c478a3fd3
Don Cooper <don_cooper@hotmail.com>
2017-06-28 17:08:29 -0400

    Giphy showing, exercise complete

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,11 +3,9 @@ class PokemonController < ApplicationController
   def show
     res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}", followlocation: true)
     body = JSON.parse(res.body)
-    render json: {
-      id:     body["id"],
-      name:   body["name"],
-      types:  body["types"]
-    }
+    res2 = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=a4fb0f987d914fc3ad55228ddf7616ee&q=#{body["name"]}&rating=g", followlocation: true)
+    body2 = JSON.parse(res2.body)
+    render inline: '<iframe src='+body2["data"][0]["embed_url"]+' width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
   end
 
 end
